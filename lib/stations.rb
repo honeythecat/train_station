691868dded4d68422689eb8a3ea1dfe68f09e4d4
Heather Anderson & Tyler Whitsett <student@epicodus.com>
2015-01-21 16:14:15 -0800

    Add specs and code.

--- lib/stations.rb
+++ lib/stations.rb
@@ -1,10 +1,10 @@
 class Station
 
-  attr_reader(:station_name, :station_id)
+  attr_reader(:station_name, :line_id)
 
   define_method(:initialize) do |attributes|
     @station_name = attributes.fetch(:station_name)
-    @station_id = attribues.fetch(:station_id)
+    @line_id = attributes.fetch(:line_id)
   end
 
   define_singleton_method(:all) do
@@ -12,18 +12,18 @@ class Station
     stations = []
     returned_stations.each() do |station|
       station_name = station.fetch("station_name")
-      station_id = station.fetch("station_id").to_i()
-      stations.push(Station.new({:station_name => station_name, :station_id => station_id}))
+      line_id = station.fetch("line_id").to_i()
+      stations.push(Station.new({:station_name => station_name, :line_id => line_id}))
     end
     stations
   end
 
   define_method(:save) do
-    DB.exec("INSERT INTO stations (station_name, station_id) VALUES ('#{@station_name}', #{@station_id});")
+    DB.exec("INSERT INTO stations (station_name) VALUES ('#{@station_name}');")
+    @line_id = result.first().fetch("line_id").to_i()
   end
 
   define_method(:==) do |first_station|
-binding.pry
-    self.station_name().==(first_station.station_name.())
+    self.station_name().==(first_station.station_name.()).&(self.line_id().==(first_station.line_id()))
   end
 end
--- spec/stations_spec.rb
+++ spec/stations_spec.rb
@@ -10,14 +10,14 @@ describe(Station) do
 
   describe("#station_name") do
     it("returns the station name") do
-      test_station = Station.new({:station_name => "Kitty Junction"})
+      test_station = Station.new({:station_name => "Kitty Junction", :line_id => 1})
       expect(test_station.station_name()).to(eq("Kitty Junction"))
     end
   end
 
   describe("#save") do
     it("saves a station name") do
-      test_station = Station.new({:station_name => "Kitty Junction"})
+      test_station = Station.new({:station_name => "Kitty Junction", :line_id => 1})
       test_station.save()
       expect(Station.all()).to(eq([test_station]))
     end
@@ -25,11 +25,18 @@ describe(Station) do
 
   describe("#==") do
     it("is the same station if it has the same station name") do
-      test_station1 = Station.new({:station_name => "Kitty Junction"})
+      test_station1 = Station.new({:station_name => "Kitty Junction", :line_id => 1})
       test_station1.save()
-      test_station2 = Station.new({:station_name => "Kitty Junction"})
+      test_station2 = Station.new({:station_name => "Kitty Junction", :line_id => 1})
       test_station2.save()
       expect(test_station1).to(eq(test_station2))
+    end
+  end
+
+  describe("#line_id") do
+    it("returns the line ID") do
+      test_station = Station.new({:station_name => "Kitty Junction", :line_id => 1})
+      expect(test_station.line_id()).to(eq(1))
     end
   end
 
